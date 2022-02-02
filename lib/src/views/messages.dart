550c6b7cafc60bf1f7d1294dad39a9e8021c2360
Ian Wadham <iandw.au@gmail.com>
2022-02-02 15:12:25 +1100

    Add functions to ask the user a question or provide information.

---
+++ lib/src/views/messages.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+Future<bool> questionMessage(
+  BuildContext context,
+  String heading,
+  String question,
+  {
+  String okText     = 'Yes',
+  String cancelText = 'No',
+  }
+) async
+{
+  // Set up the buttons.
+  Widget okButton = FlatButton(
+    child: Text(okText),
+    onPressed: () {
+      print('User pressed $okText button in questionMessage()');
+      Navigator.of(context).pop(true);	// Dismiss the dialog box.
+    },
+  );
+  Widget cancelButton = FlatButton(
+    child: Text(cancelText),
+    onPressed: () {
+      print('User pressed $cancelText button in questionMessage()');
+      Navigator.of(context).pop(false);	// Dismiss the dialog box.
+    },
+  );
+  // Set up the AlertDialog.
+  AlertDialog alert = AlertDialog(
+    title:   Text(heading),
+    content: Text(question),
+    actions: [
+      cancelButton,
+      okButton,
+    ],
+  );
+
+  bool reply = await showDialog(
+      context: context,
+      barrierDismissible: false,	// User must tap button, not background.
+      builder: (BuildContext context)
+      {
+        return alert;
+      },
+    );
+  return reply;
+}
+
+Future<void> infoMessage
+(
+  BuildContext context,
+  String heading,
+  String information,
+  {
+  String okText     = 'OK',
+  }
+)
+async
+{
+  // Set up the buttons.
+  Widget okButton = FlatButton(
+    child: Text(okText),
+    onPressed: () {
+      Navigator.of(context).pop();	// Dismiss the dialog box.
+    },
+  );
+  // Set up the AlertDialog.
+  AlertDialog alert = AlertDialog(
+    title:   Text(heading),
+    content: Text(information),
+    actions: [
+      okButton,
+    ],
+  );
+  // Show the info message.
+  bool? reply = await showDialog(
+    context: context,
+    barrierDismissible: false,		// User must tap button, not background.
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
