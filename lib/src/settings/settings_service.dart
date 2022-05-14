caaeaea494431a690287c46b15de1e011a39e3e7
Ian Wadham <iandw.au@gmail.com>
2022-05-14 11:10:23 +1000

    Comment out the debugging messages in SettingsService.

--- lib/src/settings/settings_service.dart
+++ lib/src/settings/settings_service.dart
@@ -13,38 +13,38 @@ class SettingsService
 
   ThemeMode loadThemeMode(String themeModeKey, ThemeMode defaultOption) {
     int index = _Prefs.read<int>(themeModeKey) ?? defaultOption.index;
-    print('Prefs: Found ThemeMode = $index: ${ThemeMode.values[index]}');
+    // print('Prefs: Found ThemeMode = $index: ${ThemeMode.values[index]}');
     return ThemeMode.values[index];
   }
 
   void storeThemeMode(String themeModeKey, ThemeMode newThemeMode) {
     _Prefs.write(themeModeKey, newThemeMode.index);
     int index = newThemeMode.index;
-    print('Prefs: Wrote ThemeMode = $index: ${ThemeMode.values[index]}');
+    // print('Prefs: Wrote ThemeMode = $index: ${ThemeMode.values[index]}');
   }
 
   Difficulty loadDifficulty(String difficultyKey, Difficulty defaultOption) {
     int index = _Prefs.read<int>(difficultyKey) ?? defaultOption.index;
-    print('Prefs: Found Difficulty = $index: ${Difficulty.values[index]}');
+    // print('Prefs: Found Difficulty = $index: ${Difficulty.values[index]}');
     return Difficulty.values[index];
   }
 
   void storeDifficulty(String difficultyKey, Difficulty newDifficulty) {
     _Prefs.write(difficultyKey, newDifficulty.index);
     int index = newDifficulty.index;
-    print('Prefs: Wrote Difficulty = $index: ${Difficulty.values[index]}');
+    // print('Prefs: Wrote Difficulty = $index: ${Difficulty.values[index]}');
   }
 
   Symmetry loadSymmetry(String symmetryKey, Symmetry defaultSymmetry) {
     int index = _Prefs.read<int>(symmetryKey) ?? defaultSymmetry.index;
-    print('Prefs: Found Symmetry = $index: ${Symmetry.values[index]}');
+    // print('Prefs: Found Symmetry = $index: ${Symmetry.values[index]}');
     return Symmetry.values[index];
   }
 
   void storeSymmetry(String symmetryKey, Symmetry newSymmetry) {
     _Prefs.write(symmetryKey, newSymmetry.index);
     int index = newSymmetry.index;
-    print('Prefs: Wrote Symmetry = $index: ${Symmetry.values[index]}');
+    // print('Prefs: Wrote Symmetry = $index: ${Symmetry.values[index]}');
   }
 
   int loadInt(String key, int defaultOption) {
