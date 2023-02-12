60cf16eee7ba76dfe7158d12740935512ce702bf
Ian Wadham <iandw.au@gmail.com>
2023-02-12 13:06:15 +1100

    Redo TimerWidget as a text-display triggered by Puzzle's Provider.

--- lib/src/views/timer_widget.dart
+++ lib/src/views/timer_widget.dart
@@ -1,50 +1,35 @@
-import 'package:flutter/foundation.dart';
-import 'dart:async';		// Needed to compile Timer and _ticker.
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../models/puzzle.dart';
 
-  // Clock starts whenever the user decides to start solving a Puzzle.
-  // Clock stops whenever he/she finishes solving the Puzzle or abandons it.
-  Stopwatch  _solutionTime = Stopwatch();
-  Timer?     _ticker = null;		// Null when there is no Timer running.
-  String     solutionTimeDisplay = '';	// Updated by Timer, once per second.
+class TimerWidget extends StatelessWidget
+{
+  // Show or hide a game-timer from the Puzzle class. If the timer has not
+  // yet started or has not yet counted off one second, its value is String '',
+  // so this widget will not display anything then...
 
-  void startClock()
-  {
-    // return;			// Hook for testing BoardView, CellView, etc.
+  TimerWidget({required this.visible,		// User's setting...
+               Key? key}) : super(key: key);
 
-    // TODO - Test to make sure that this assert does not trigger.
-    assert(_ticker == null, 'ASSERT ERROR startClock(): _ticker is NOT null.');
-    print('START THE CLOCK!!!');
-    _solutionTime.reset();
-    _solutionTime.start();
-    // Start a 1-second ticker, but only if it is null (not already running).
-    _ticker ??= Timer.periodic(const Duration(seconds: 1), (_ticker)
-      {
-        // One tick per second.
-        Duration t = _solutionTime.elapsed;
-        solutionTimeDisplay = '${t.toString().split('.').first}'; // (h)h:mm:ss
-        if (t < Duration(hours: 1)) {
-          // Remove leading zero(s) and colon.
-          int xxx = solutionTimeDisplay.indexOf(':') + 1;
-          if (t < Duration(minutes: 10)) {
-            xxx++;
-          }
-          solutionTimeDisplay = solutionTimeDisplay.substring(xxx /* to end */);
-        }
-        notifyListeners();			// Display the time (if reqd).
-      }
-    );
+  final bool visible;
+
+  late Puzzle puzzle;		// Located by Provider's watch<Puzzle> function.
+
+  @override
+  Widget build(BuildContext context) {
+
+    Puzzle puzzle   = context.watch<Puzzle>();
+    String userTime = puzzle.userTimeDisplay;
+
+    if (visible) {
+      return Text(userTime,	// Show the timer, if it has started, else ''.
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                 );
+    }
+    else {
+      return Text('');		// Timer is hidden: nothing to paint.
+    }
   }
-
-  void stopClock()
-  {
-    // return;			// Hook for testing BoardView, CellView, etc.
-
-    // TODO - Test to make sure that this assert does not trigger.
-    assert(_ticker != null, 'ASSERT ERROR stopClock(): _ticker IS NULL.');
-    _solutionTime.stop();
-    _ticker?.cancel();
-    _ticker = null;
-    print('STOP THE CLOCK!!!');
-  }
-
+}
