13a164856bba30b51eede4f1e9f8e24c9c2930d9
Ian Wadham <iandw.au@gmail.com>
2023-03-03 16:15:00 +1100

    Improve grid-lines in BoardGridView2D. Add cage-painting code.

--- lib/src/views/board_grid_view.dart
+++ lib/src/views/board_grid_view.dart
@@ -35,22 +35,31 @@ class BoardGridView2D extends StatelessWidget
 
     final gameTheme = context.watch<GameTheme>();
     _calculateEdgeLines();
-    // TODO - RepaintBoundary seems to be ESSENTIAL to stop GridPainter firing
-    //        continually when a cell is tapped but the grid stays the same size
-    //        and colour. It also stops GridPainter firing whenever the pointer
-    //        moves out of the desktop window.
-    return RepaintBoundary(child: CustomPaint(
-      painter: GridPainter(puzzleMap, boardSide, edgesEW, edgesNS,
-                       gameTheme.thinLineColor, gameTheme.boldLineColor),),
+    markCageBoundaries(puzzleMap);
+
+    // RepaintBoundary seems to be essential to stop GridPainter firing
+    // continually whenever a cell is tapped but the grid is unchanged.
+    // It also stops GridPainter firing whenever the pointer moves out
+    // of the Puzzle's desktop window (on a MacOS machine).
+
+    return RepaintBoundary(
+      child: CustomPaint(
+        painter: GridPainter(puzzleMap, boardSide, edgesEW, edgesNS,
+                   gameTheme.thinLineColor, gameTheme.boldLineColor,
+                   cagePerimeters, gameTheme.cageLineColor,
+                   gameTheme.emptyCellColor,
+        ),
+      ),
     );
 
     // TODO - Do all calculations and cleanup of Lists inside CustomPainter?
     //        Check on what Tictactoe's RoughGrid does...
+    // TODO - Tidy up and shorten the code for painting the text in cage-labels.
 
   } // End Widget build
 
-  /*static*/ List<int> edgesEW = [];	// East-West edges of cells.
-  /*static*/ List<int> edgesNS = [];	// North-South edges of cells.
+  List<int> edgesEW = [];	// East-West edges of cells.
+  List<int> edgesNS = [];	// North-South edges of cells.
 
   void _calculateEdgeLines()
   {
@@ -282,7 +291,8 @@ class GridPainter extends CustomPainter
 {
 
   GridPainter(this.puzzleMap, this.boardSide, this.edgesEW, this.edgesNS,
-              this.thinLineColor, this.boldLineColor);
+              this.thinLineColor, this.boldLineColor,
+              this.cagePerimeters, this.cageLineColor, this.emptyCellColor);
 
   final PuzzleMap puzzleMap;
   final double    boardSide;
@@ -290,6 +300,9 @@ class GridPainter extends CustomPainter
   final List<int> edgesNS;
   final Color     thinLineColor;
   final Color     boldLineColor;
+  final List<List<int>> cagePerimeters;
+  final Color     cageLineColor;
+  final Color     emptyCellColor;
 
   @override
   void paint(Canvas canvas, Size size)
@@ -315,33 +328,162 @@ class GridPainter extends CustomPainter
     boldLinePaint.strokeWidth  = cellSide / 15.0;
 
     // WILL BE NEEDED LATER...
-    // cageLinePaint.strokeWidth  = cellSide / 20.0;
     // highlight.strokeWidth      = cellSide * paintingSpecs.highlightInset;
 
     // Draw light and dark edges of puzzle-area, as required by the puzzle type.
-    int nEdges = sizeY * (sizeX + 1);
-    for (int i = 0; i < nEdges; i++) {
-      double oX = (i~/(sizeY + 1)) * cellSide;
-      double oY = (i%(sizeY + 1))  * cellSide;
-      int paintType = edgesEW[i];
-      if (paintType > 0) {
-        Paint p = (paintType == 1) ? thinLinePaint : boldLinePaint;
-        canvas.drawLine(Offset(oX, oY), Offset(oX + cellSide, oY), p);
-      }
-      oX = (i~/sizeY) * cellSide;
-      oY = (i%sizeY)  * cellSide;
-      paintType = edgesNS[i];
-      if (paintType > 0) {
-        Paint p = (paintType == 1) ? thinLinePaint : boldLinePaint;
-        canvas.drawLine(Offset(oX, oY), Offset(oX, oY + cellSide), p);
+    // Paint all the light edges first (edgeType 1) then all the dark edges on
+    // top of them (edgeType 2), avoiding some tiny glitches where the dark and
+    // light lines cross, also skipping both calculation and painting for cells
+    // that are designated UNUSABLE (e.g. in Samurai-style puzzles);
+
+    int    nEdges = sizeY * (sizeX + 1);
+    double oX     = 0.0;
+    double oY     = 0.0;
+
+    for (int edgeType in [1, 2]) {	// Light first, then dark.
+      Paint p = (edgeType == 1) ? thinLinePaint : boldLinePaint;
+      for (int i = 0; i < nEdges; i++) {
+        if (edgesEW[i] == edgeType) {
+          oX = (i~/(sizeY + 1)) * cellSide;
+          oY = (i%(sizeY + 1))  * cellSide;
+          canvas.drawLine(Offset(oX, oY), Offset(oX + cellSide, oY), p);
+        }
+        if (edgesNS[i] == edgeType) {
+          oX = (i~/sizeY) * cellSide;
+          oY = (i%sizeY)  * cellSide;
+          canvas.drawLine(Offset(oX, oY), Offset(oX, oY + cellSide), p);
+        }
       }
     }
+
+    // Paint cages if there are any: for Mathdoku and Killer Sudoku only.
+    if (cagePerimeters.isEmpty) {
+      return;
+    }
+
+    Paint cageLinePaint = Paint()		// Style for cage outlines.
+      ..color = cageLineColor
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeJoin  = StrokeJoin.round;
+    Paint emptyCellPaint = Paint()		// Colour of unfilled 2D cells.
+      ..color = emptyCellColor
+      ..style = PaintingStyle.fill;
+
+    cageLinePaint.strokeWidth  = cellSide / 20.0;
+
+    // In Mathdoku or Killer Sudoku, paint the outlines and labels of the cages.
+    if (puzzleMap.cageCount() > 0) {
+      paintCages(canvas, puzzleMap.cageCount(), cellSide,
+                 boldLinePaint, cageLinePaint, emptyCellPaint);
+    }
+
   }
 
   @override
   bool shouldRepaint(GridPainter oldDelegate)
   {
     return oldDelegate.boardSide != boardSide;
+  }
+
+  void paintCages(Canvas canvas, int cageCount, double cellSide,
+                  Paint labelPaintFg, Paint cageLinePaint, Paint labelPaintBg)
+  {
+    // PaintingSpecs2D paintingSpecs  = paintingSpecs2D;
+    PuzzleMap       map            = puzzleMap;
+
+// TODO - Can we make this a list of Path ... i.e. List<Path>? If so, we would
+//        have to introduce the insets in some other way...
+    // List<List<int>> cagePerimeters = paintingSpecs.cagePerimeters;
+
+    double inset = cellSide/12.0;
+    List<Offset> corners = [Offset(inset, inset),			// NW
+                            Offset(cellSide - inset, inset),		// NE
+                            Offset(cellSide - inset, cellSide - inset),	// SE
+                            Offset(inset, cellSide - inset)];		// SW
+
+    // Paint lines to connect lists of right-turn and left-turn points in cage
+    // perimeters. Lines are inset within cage edges and have a special colour.
+    for (List<int> perimeter in cagePerimeters) {
+      Offset? startLine;	// Initially null.
+      for (Pair point in perimeter) {
+        int cell          = point >> lowWidth;
+        int corner        = point & lowMask;
+        int i             = puzzleMap.cellPosX(cell);
+        int j             = puzzleMap.cellPosY(cell);
+        Offset cellOrigin = /*topLeft +*/ Offset(i * cellSide, j * cellSide);
+        Offset endLine    = cellOrigin + corners[corner];
+        if (startLine != null) {
+          canvas.drawLine(startLine, endLine, cageLinePaint);
+        }
+        startLine = endLine;		// Get ready for the next line.
+      }
+    }
+
+    // Paint the cage labels.
+    for (int cageNum = 0; cageNum < cageCount; cageNum++) {
+      String cageLabel  = getCageLabel(map, cageNum);
+      if (cageLabel == '') {
+        continue;		// Don't paint labels on size 1 cages (Givens).
+      }
+
+      int labelCell     = map.cageTopLeft(cageNum);
+      int cellX         = map.cellPosX(labelCell);
+      int cellY         = map.cellPosY(labelCell);
+      double textSize   = cellSide / 6.0;
+      Offset cellOrigin = /*topLeft +*/ Offset(cellX * cellSide, cellY * cellSide);
+      Offset inset      = Offset(cellSide/20.0, cellSide/20.0);
+      print('Paint cage $cageNum label $cageLabel');
+      paintTextString(canvas, cageLabel,
+                                    textSize, cellOrigin + inset,
+                                    labelPaintFg, labelPaintBg);
+    }
+  }
+
+  String getCageLabel (PuzzleMap map, int cageNum)
+  {
+    bool killerStyle = (map.specificType == SudokuType.KillerSudoku);
+    if (map.cage(cageNum).length < 2) {
+	return '';		// 1-cell cages are displayed as Givens (clues).
+    }
+
+    // TODO - Operators or cell-values should be randomly revealed in Hints.
+    // TODO - Operators should all be revealed when a solution is reached.
+
+    // No operator is shown in KillerSudoku, nor in Blindfold Mathdoku.
+    String cLabel = map.cageValue(cageNum).toString();
+    if ((! killerStyle) && (! map.hideOperators)) {
+      int opNum = map.cageOperator(cageNum).index;
+      cLabel = cLabel + " /-x+".substring(opNum, opNum + 1);
+    }
+    print('Cage Label $cLabel, cage $cageNum');
+    return cLabel;
+  }
+
+  void paintTextString(Canvas canvas, String textString, double textSize,
+                       Offset offset, Paint foreground, Paint background)
+  {
+    double padding = 0.25 * textSize;
+    const double baseSize = 60.0;	// Base-size for scaling text symbols.
+    // A text-painter for painting Sudoku symbols on a Canvas.
+    final TextPainter textPainter = TextPainter(
+          textAlign: TextAlign.center,
+          maxLines: 1,
+          textDirection: TextDirection.ltr);
+
+    TextStyle stringStyle = TextStyle(	// Make sure the string picks up the
+      color:      foreground.color,	// text color on light/dark change.
+      fontSize:   baseSize,
+      fontWeight: FontWeight.bold);
+    textPainter.text = TextSpan(style: stringStyle, text: textString);
+    textPainter.textScaleFactor = textSize / baseSize;
+    textPainter.layout();
+
+    Rect textRect  = Rect.fromPoints(offset, offset +
+                     Offset(padding + textPainter.width, textSize * 5.0 / 4.0));
+    canvas.drawRect(textRect, background);
+    // Need padding at both ends of string, so inset by padding / 2.0;
+    textPainter.paint(canvas, offset + Offset(padding / 2.0, 0.0));
   }
 
 } // End class GridPainter.
